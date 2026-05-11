1b23e6293c981ec6f23ababd3255adcd4a6bfb01
Matheus <matheusn.costa27@gmail.com>
2026-05-11 14:13:50 -0300

    matheus-1105-alteracoes

--- pacotes.js
+++ pacotes.js
@@ -11,3 +11,53 @@ dateInput.addEventListener('change', function() {
       this.classList.remove('oinputCor-preto');       //Remove a classe (input_data_preto_realizar_reserva)
     }
   });
+
+  function abrirModal(elemento) {
+    var modal = document.getElementById("meuModal");
+    var imgDestaque = document.getElementById("imgDestaque");
+    var listaMiniaturas = document.getElementById("listaMiniaturas");
+    
+    modal.style.display = "flex";
+    imgDestaque.src = elemento.src;
+    
+   
+    var containerCard = elemento.closest('div').parentElement; 
+    var todasAsImagens = containerCard.querySelectorAll('img');
+    
+    listaMiniaturas.innerHTML = "";
+    
+    todasAsImagens.forEach(function(img) {
+        
+        if(!img.classList.contains('wifi') && !img.classList.contains('wifi1')) {
+            var novaMini = document.createElement("img");
+            novaMini.src = img.src;
+            novaMini.onclick = function() {
+                imgDestaque.src = this.src;
+            };
+            listaMiniaturas.appendChild(novaMini);
+        }
+    });
+}
+
+function fecharModal() {
+    document.getElementById("meuModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("meuModal");
+    if (event.target == modal) {
+        fecharModal();
+    }
+}
+
+function fecharModal() {
+    document.getElementById("meuModal").style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    var modal = document.getElementById("meuModal");
+    if (event.target == modal) {
+        fecharModal();
+    }
+}
